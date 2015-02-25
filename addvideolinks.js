7bf6cca208658edc8cc0bd589083002fd65c8641
James F Odeh <thenamesviper@gmail.com>
2015-02-25 01:41:20 -0800

    Fixed linking

--- addvideolinks.js
+++ addvideolinks.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
-	for(i = 0; i<13; i++) {
+	for(i = 0; i<14; i++) {
 		$("ul").append(	"<li>" +
 						"<a href = 'http://www.youtube.com'>" +
+						"<div>" +
 							"<div class = 'videopicture'>"	 +
 								"<img src = 'https://i.ytimg.com/vi/mENDYhfxH-o/default.jpg'/>" +
 							"</div>" +
+						
+						"<a href = 'http://www.youtube.com'>" +
 							"<div class = 'videodescription'>" +
 								"<p>This is the title of this video</p>" +
 								"<p>By John and Jane Doe</p>" +
 								"<p>54,454 views</p>" +
-						"</div>" +
+							"</div>" +
+						"</div" +
 						"</a>" +
 						"</li>" 
 			);
